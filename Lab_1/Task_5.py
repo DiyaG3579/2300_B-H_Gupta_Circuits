b3b879947330b6758bdeb0a7bc2f3d137cc08faf
DiyaG3579 <diya@wustl.edu>
2026-09-25 13:45:40 -0500

    Added comments that I forgot to add

--- Lab_1/Task_5.py
+++ Lab_1/Task_5.py
@@ -1,13 +1,18 @@
+#Importing math, because you know that is this task's whole point
 import math
 
+#Starting with e = 0, because x^0 = 1 will be the first term
 e = 0
+#Getting the code (The first term will be num = 0, so it will always run it one less than submitted)
 number = int(input("How many terms would you like your approximation to be done with: "))
 x = 0
 
+#Then the whole thing, the actual calculations
 while x < number:
     top = math.pow(1,x)
     bottom = math.factorial(x)
     e = e + (top/bottom)
     x = x + 1
 
+#Finally returning an answer
 print("e =", e)  
