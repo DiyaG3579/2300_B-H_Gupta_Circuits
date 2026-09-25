28d8ccde35a7ee6adb13eff898506fbd8e1b11fe
DiyaG3579 <diya@wustl.edu>
2026-09-25 13:30:38 -0500

    Testing

--- Lab_1/Task_1.py
+++ Lab_1/Task_1.py
@@ -1,12 +1,16 @@
+#Importing everything that I need
 import math
 
+#Setting up the variables that will come into play - and establishing what x is (For the number of tires)
 pi = 3.141592
 r = float(input('Enter the Radius of Your Sphere: '))
 x = 1
 
+#Adding instructions and input
 print('Press 1 to Calculate Surface Area; Press 2 to Calculate Volume')
 quantity = int(input('What would you like to calculate: '))
 
+#Loop to make sure they input something correctly
 while x < 4:
     if quantity == 1:
         area = (4 * pi * r * r)
@@ -18,6 +22,6 @@ while x < 4:
         x = 4
     else: 
         quantity = int(input('Please select 1 or 2 - (1 for Surface Area and 2 for Volume): '))
-
+#Exiting out the code
 if x >= 4:
     print('You have reached the maximum number of attempts, please try again by re-running the program')
---
+++ Lab_1/Task_3.py
@@ -0,0 +1,3 @@
+#Using the for loop that was given in the lab
+for x in range(0,104,4):
+   print(x)
