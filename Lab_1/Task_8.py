e23cdd128a6ec42688c48431d34a0a1333d0b212
Magz <brooke-hodge@wustl.edu>
2026-09-25 20:22:47 +0000

    Added commets to Task8

--- Lab_1/Task_8.py
+++ Lab_1/Task_8.py
@@ -1,4 +1,4 @@
 def truncate (number, decimal):
-    factor = 10**decimal
-    return int(number*factor)/factor
+    factor = 10**decimal #Find the factor needed for the number of decimal places 
+    return int(number*factor)/factor #Move the decimal, remove the extra digits, and then mmove the decimal back
 print(truncate(3.141596,4))
